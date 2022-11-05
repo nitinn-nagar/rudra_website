83f1dbcc35ff02e5ef9f430f528745861a2a52be
Nitin <nnagar141@gmail.com>
2022-11-05 18:19:37 +0530

    achievements

--- js/script.js
+++ js/script.js
@@ -1,24 +1,12 @@
-//const btn = document.getElementById('menu-btn');
-//const overlay = document.getElementById('overlay');
-//const menu = document.getElementById('mobile-menu');
-//btn.addEventListener('click', navToggle);
+const btn = document.getElementById('menu-btn');
+const overlay = document.getElementById('overlay');
+const menu = document.getElementById('mobile-menu');
 
-const slidesContainer = document.getElementById("slides-container");
-const slide = document.querySelector(".slide");
-const prevButton = document.getElementById("slide-arrow-prev");
-const nextButton = document.getElementById("slide-arrow-next");
-nextButton.addEventListener("click", (event) => {
-    const slideWidth = slide.clientWidth;
-    slidesContainer.scrollLeft += slideWidth;
-});
-prevButton.addEventListener("click", () => {
-    const slideWidth = slide.clientWidth;
-    slidesContainer.scrollLeft -= slideWidth;
-});
 
-//function navToggle() {
-//  btn.classList.toggle('open');
-//overlay.classList.toggle('overlay-show');
-//document.body.classList.toggle('stop-scrolling');
-//menu.classList.toggle('show-menu');
-//}
+btn.addEventListener('click', () => {
+    console.log("ham was clicked");
+    btn.classList.toggle('open');
+    overlay.classList.toggle('overlay-show');
+    document.body.classList.toggle('stop-scrolling');
+    menu.classList.toggle('show-menu');
+});
