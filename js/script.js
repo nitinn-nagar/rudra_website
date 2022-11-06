606e0469599d64fe00250aa43f4a4b8048d771f8
Nitin <nnagar141@gmail.com>
2022-11-06 10:17:49 +0530

    contact us page completed, form remaining

--- js/script.js
+++ js/script.js
@@ -10,3 +10,23 @@ btn.addEventListener('click', () => {
     document.body.classList.toggle('stop-scrolling');
     menu.classList.toggle('show-menu');
 });
+
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+    let parent = this.parentNode;
+    parent.classList.add("focus");
+}
+
+function blurFunc() {
+    let parent = this.parentNode;
+    if (this.value == "") {
+        parent.classList.remove("focus");
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener("focus", focusFunc);
+    input.addEventListener("blur", blurFunc);
+});
